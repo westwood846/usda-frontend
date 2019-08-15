8ac4e43ae20d9639b39e09e1b068935317e9a239
turbopope <PapstDonB@Googlemail.com>
2019-08-15 21:38:45 +0800

    Add non-functioning data source radio inputs

--- src/Search/SearchBar.js
+++ src/Search/SearchBar.js
@@ -6,16 +6,28 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleDataSourceChange = this.handleDataSourceChange.bind(this);
+
+    this.state = {
+      dataSource: 'STANDARD_REFERENCE'
+    }
   }
   
   handleInputChange = (event) => {
     this.props.search(event.target.value);
   }
 
+  handleDataSourceChange = (event) => {
+    this.setState({dataSource: event.target.value})
+  }
+
   render() {
     return (
       <div className="SearchBar">
-        <label>Search: <input type="text" value={this.props.searchQuery} onChange={this.handleInputChange} autoFocus/></label>
+        <label>Search: <input type="text" className="queryInput" value={this.props.searchQuery} onChange={this.handleInputChange} autoFocus/></label><br/>
+        <label className="dataSourceInputLabel"><input type="radio" className="dataSourceInput" value="BOTH" checked={this.state.dataSource === 'BOTH'} onChange={this.handleDataSourceChange}></input>Both</label>
+        <label className="dataSourceInputLabel"><input type="radio" className="dataSourceInput" value="STANDARD_REFERENCE" checked={this.state.dataSource === 'STANDARD_REFERENCE'} onChange={this.handleDataSourceChange}></input>Standard Reference</label>
+        <label className="dataSourceInputLabel"><input type="radio" className="dataSourceInput" value="BRANDED_FOOD_PRODUCTS" checked={this.state.dataSource === 'BRANDED_FOOD_PRODUCTS'} onChange={this.handleDataSourceChange}></input>Branded Food Products</label>
       </div>
     );
   }
