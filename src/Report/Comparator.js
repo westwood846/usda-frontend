3d08f369021df4035fc40c3f891e268454d678d8
turbopope <PapstDonB@Googlemail.com>
2019-08-17 21:48:22 +0800

    Add bubble tea comparator

--- src/Report/Comparator.js
+++ src/Report/Comparator.js
@@ -3,11 +3,13 @@ import { times } from 'lodash';
 
 const BANANA_FACTOR = 105;
 const BREAD_FACTOR = 67;
+const BUBBLE_TEA_FACTOR = 350;
 
 class Comparator extends Component {
   render() {
     let bananas = Math.round(this.props.calories / BANANA_FACTOR);
     let bread = Math.round(this.props.calories / BREAD_FACTOR);
+    let bubbleTeas = Math.round(this.props.calories / BUBBLE_TEA_FACTOR);
     
     return (
       <div className="Comparator">
@@ -15,10 +17,13 @@ class Comparator extends Component {
           {this.props.calories} kcal equal...
         </div>
         <div className="emoji-comparator">
-          {times(bananas, n => '🍌')} {bananas}&nbsp;medium&nbsp;bananas&nbsp;(118g)
+          {times(bananas, n => '🍌')} {bananas}&nbsp;Medium&nbsp;Bananas&nbsp;(118g)
         </div>
         <div className="emoji-comparator">
-          {times(bread, n => '🍞')} {bread}&nbsp;slices&nbsp;of&nbsp;white&nbsp;bread&nbsp;(28g)
+          {times(bread, n => '🍞')} {bread}&nbsp;Slices&nbsp;of&nbsp;White&nbsp;Bread&nbsp;(28g)
+        </div>
+        <div className="emoji-comparator">
+          {times(bubbleTeas, n => '🍹')} {bubbleTeas}&nbsp;Cups&nbsp;of&nbsp;Bubble&nbsp;Tea&nbsp;
         </div>
       </div>
     )
