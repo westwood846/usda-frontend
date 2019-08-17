e7b891c88ac72c7c1cf3444732f9e44484eb58c1
turbopope <PapstDonB@Googlemail.com>
2019-08-17 17:08:17 +0800

    Don't draw labels if slice is too thin

--- src/Report/ProximatesChart.js
+++ src/Report/ProximatesChart.js
@@ -33,24 +33,29 @@ export default class ProximatesChart extends PureComponent {
       { name: 'Monounsaturated fat', value: monoSaturatedFats },
       { name: 'Polyunsaturated fat', value: polySaturatedFats },
       { name: 'Saturated fat', value: saturatedFats },
-      { name: 'Dietary fiber', value: dietaryFiber },
+      { name: 'Dietary Fiber', value: dietaryFiber },
       { name: 'Sugar', value: sugar },
       { name: 'Other Carbohydrates', value: otherCarbs },
       { name: 'Protein', value: protein },
       { name: 'Water', value: water },
     ];
 
+    let sliceWideEnough = (props) => {
+      return props.percent > 0.03;
+    }
+
     let label = (props) => {
       let { cx, cy, midAngle, innerRadius, outerRadius, value, index, fill } = props;
+
+      if (!sliceWideEnough(props)) return null;
+
       const RADIAN = Math.PI / 180;
       const radius = 25 + innerRadius + (outerRadius - innerRadius);
       const x = cx + radius * Math.cos(-midAngle * RADIAN);
       const y = cy + radius * Math.sin(-midAngle * RADIAN);
       value = new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 1 }).format(value);
 
-      return (
-        <text x={x} y={y} fill={fill} textAnchor={x > cx ? "start" : "end"} dominantBaseline="central">{data02[index].name} {value}</text>
-      );
+      return <text x={x} y={y} fill={fill} textAnchor={x > cx ? "start" : "end"} dominantBaseline="central">{data02[index].name} {value}</text>;
     }
 
     let colors = [
@@ -65,7 +70,7 @@ export default class ProximatesChart extends PureComponent {
         <Pie data={data01} dataKey="value" cx={250} cy={150} outerRadius={60} fill="#8884d8">{
           data01.map((entry, index) => <Cell key={``} fill={colors[index][0]} />)
         }</Pie>
-        <Pie data={data02} dataKey="value" cx={250} cy={150} innerRadius={70} outerRadius={90} fill="red" label={label}>{
+        <Pie data={data02} dataKey="value" cx={250} cy={150} innerRadius={70} outerRadius={90} fill="red" labelLine={false} label={label}>{
           data02.map((entry, index) => <Cell key={``} fill={flatten(colors)[index]} />)
         }</Pie>
       </PieChart>
