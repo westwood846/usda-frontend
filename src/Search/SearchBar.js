b7defe76de8f0419c386a79558a4b33763b7642d
turbopope <PapstDonB@Googlemail.com>
2019-08-17 22:22:02 +0800

    Lift search state into url

--- src/Search/SearchBar.js
+++ src/Search/SearchBar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { setQuery, setDataSource, search } from '../actions';
 import { dataSourceIdentifiers, decodeDataSourceIdentifier } from '../usda';
+import { push } from 'connected-react-router'
 
 class SearchBar extends Component {
   constructor(props) {
@@ -9,14 +10,20 @@ class SearchBar extends Component {
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleDataSourceChange = this.handleDataSourceChange.bind(this);
   }
+
+  stringifyState = (query=this.props.searchQuery, dataSource=this.props.searchDataSource) => {
+    return `?query=${query}&dataSource=${dataSource}`;
+  }
   
   handleInputChange = (event) => {
     this.props.setQuery(event.target.value);
+    this.props.push(this.stringifyState(event.target.value));
     if (this.props.searchQuery) this.props.search();
   }
   
   handleDataSourceChange = (event) => {
     this.props.setDataSource(event.target.value);
+    this.props.push(this.stringifyState(undefined, event.target.value));
     if (this.props.searchQuery) this.props.search();
   }
 
@@ -45,7 +52,8 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = {
   setQuery,
   setDataSource,
-  search
+  search,
+  push
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchBar);
