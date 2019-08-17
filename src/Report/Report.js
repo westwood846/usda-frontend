abed983c33e4a6a87d46c4e8c70dd6fe8b902e08
turbopope <PapstDonB@Googlemail.com>
2019-08-17 11:58:00 +0800

    Put most important nutrient data into a nicer table

--- src/Report/Report.js
+++ src/Report/Report.js
@@ -1,15 +1,77 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { sortNutrientsByGroup, groupByNutrientGroup } from '../usda';
+import { sortNutrientsByGroup, groupByNutrientGroup, getNutrient, referenceIntake } from '../usda';
 import { NutrientTable } from './NutrientsTable';
 
 class Report extends Component {
+  datum = (key, precision=0) => {
+    let nutrient = getNutrient(this.props.report, key);
+    return `${Number(nutrient.value).toFixed(precision)} ${nutrient.unit}`;
+  }
+
+  reference = (key) => {
+    return `${Math.round(getNutrient(this.props.report, key).value / referenceIntake[key] * 100)} %`;
+  }
+
   render() {
     let nutrients = sortNutrientsByGroup(this.props.report.nutrients);
     let nutrientGroups = groupByNutrientGroup(nutrients);
 
     return (
       <div className="Report">
+        <h2>Proximates</h2>
+        <table className="nutTable">
+          <tbody>
+            <tr>
+              <td className="nutTable-left"><strong>Calories </strong>{this.datum('Energy')}</td>
+              <td className="nutTable-right">{this.reference('Energy')}</td>
+            </tr>
+            <tr>
+              <td className="nutTable-left"><strong>Total Fat </strong>{this.datum('Total lipid (fat)')}</td>
+              <td className="nutTable-right">{this.reference('Total lipid (fat)')}</td>
+            </tr>
+            <tr>
+              <td className="nutTable-left" className="indent-2">Saturated fat {this.datum('Fatty acids, total saturated')}</td>
+              <td className="nutTable-right">{this.reference('Fatty acids, total saturated')}</td>
+            </tr>
+            <tr>
+              <td className="nutTable-left" className="indent-2">Polyunsaturated fat {this.datum('Fatty acids, total polyunsaturated')}</td>
+              <td className="nutTable-right"></td>
+            </tr>
+            <tr>
+              <td className="nutTable-left" className="indent-2">Monounsaturated fat {this.datum('Fatty acids, total monounsaturated')}</td>
+              <td className="nutTable-right"></td>
+            </tr>
+            <tr>
+              <td className="nutTable-left"><strong>Cholesterol </strong>{this.datum('Cholesterol')}</td>
+              <td className="nutTable-right">{this.reference('Cholesterol')}</td>
+            </tr>
+            <tr>
+              <td className="nutTable-left"><strong>Sodium </strong>{this.datum('Sodium, Na')}</td>
+              <td className="nutTable-right">{this.reference('Sodium, Na')}</td>
+            </tr>
+            <tr>
+              <td className="nutTable-left"><strong>Potassium </strong>{this.datum('Potassium, K')}</td>
+              <td className="nutTable-right">{this.reference('Potassium, K')}</td>
+            </tr>
+            <tr>
+              <td className="nutTable-left"><strong>Total Carbohydrate </strong>{this.datum('Carbohydrate, by difference')}</td>
+              <td className="nutTable-right">{this.reference('Carbohydrate, by difference')}</td>
+            </tr>
+            <tr>
+              <td className="nutTable-left indent-2">Dietary fiber {this.datum('Fiber, total dietary', 1)}</td>
+              <td className="nutTable-right">{this.reference('Fiber, total dietary')}</td>
+            </tr>
+            <tr>
+              <td className="nutTable-left indent-2">Sugar {this.datum('Sugars, total', 1)}</td>
+              <td className="nutTable-right"></td>
+            </tr>
+            <tr>
+              <td className="nutTable-left"><strong>Protein </strong>{this.datum('Protein')}</td>
+              <td className="nutTable-right">{this.reference('Protein')}</td>
+            </tr>
+          </tbody>
+        </table>
         {Object.keys(nutrientGroups).map((groupKey, index) => <NutrientTable nutrients={nutrientGroups[groupKey]} name={groupKey} key={index}/>)}
       </div>
     )
--- src/usda.js
+++ src/usda.js
@@ -1,4 +1,4 @@
-import { groupBy } from 'lodash';
+import { groupBy, find } from 'lodash';
 
 
 export const groupOrder = ['Proximates', 'Vitamins', 'Minerals', 'Lipids', 'Other'];
@@ -21,4 +21,18 @@ export const dataSourceIdentifierLookup = {
 
 export const dataSourceIdentifiers = Object.keys(dataSourceIdentifierLookup);
 
-export const decodeDataSourceIdentifier = (identifier) => (dataSourceIdentifierLookup[identifier])
+export const decodeDataSourceIdentifier = (identifier) => (dataSourceIdentifierLookup[identifier]);
+
+export const getNutrient = (food, name) => find(food.nutrients, {name});
+
+export const referenceIntake = {
+  "Energy": 2000,
+  "Total lipid (fat)": 67,
+  "Fatty acids, total saturated": 20,
+  "Cholesterol": 300,
+  "Sodium, Na": 2400,
+  "Potassium, K": 3533,
+  "Carbohydrate, by difference": 300,
+  "Fiber, total dietary": 25,
+  "Protein": 50
+}
