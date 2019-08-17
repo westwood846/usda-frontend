cf094f7aeed1be728dadc72d36c06c3c64df89b4
turbopope <PapstDonB@Googlemail.com>
2019-08-17 12:12:34 +0800

    Move Analysis container next to data container

--- src/Report/Analysis.js
+++ src/Report/Analysis.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ProximatesChart from './ProximatesChart';
+import { times } from 'lodash';
+const BANANA_FACTOR = 105;
+const BREAD_FACTOR = 67;
 
 class Analysis extends Component {
   render() {
+    let calories = this.props.report.nutrients.find(nutrient => nutrient.name === "Energy");
+    let bananas = Math.round(Number(calories.value) / BANANA_FACTOR);
+    let bread = Math.round(Number(calories.value) / BREAD_FACTOR);
+
     return (
       <div className="Analysis">
         <ProximatesChart/>
         <pre><code>{JSON.stringify(this.props.report, null, 2)}</code></pre>
+        <div>
+          {calories.value} {calories.unit} equal...
+        </div>
+        <div className="emoji-comparator">
+          {times(bananas, n => '🍌')} {bananas} medium bananas (118g)
+        </div>
+        <div className="emoji-comparator">
+          {times(bread, n => '🍞')} {bread} slices of white bread (28g)
+        </div>
       </div>
     )
   }
