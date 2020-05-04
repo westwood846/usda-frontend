1be7be05f26f6ad85dfecfa14ef4bf43bada7612
westwood846 <PapstDonB@Googlemail.com>
2020-05-04 08:02:22 +0800

    Add BrowserRouter with basename

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import SearchPage from "./Search/SearchPage";
 import ReportPage from "./Report/ReportPage";
@@ -10,12 +10,14 @@ export class App extends Component {
   render() {
     return (
       <div className="App">
-        <Switch>
-          <Route path="/" exact component={SearchPage} />
-          <Route path="/search" component={SearchPage} />
-          <Route path="/report/:ndbno" component={ReportPage} />
-          <Route path="/compare" component={TablePage} />
-        </Switch>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <Switch>
+            <Route path="/" exact component={SearchPage} />
+            <Route path="/search" component={SearchPage} />
+            <Route path="/report/:ndbno" component={ReportPage} />
+            <Route path="/compare" component={TablePage} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
