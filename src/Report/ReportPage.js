e85d211da49ce5b9cd2b05c832a3139d2380ea2b
westwood846 <PapstDonB@Googlemail.com>
2020-05-30 14:37:09 +0800

    Replace mass slider with material-ui slider

--- src/Report/ReportPage.js
+++ src/Report/ReportPage.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-import InputRange from "react-input-range";
+import InputRange from "@material-ui/core/Slider";
 import "react-input-range/lib/css/index.css";
 import { get, isUndefined } from "lodash";
 
@@ -13,82 +13,75 @@ import Loading from "./Loading";
 
 import { getReport, setMass, compareInsert } from "../actions";
 
-class ReportPage extends Component {
-  constructor(props) {
-    super(props);
-    this.urlNDBNO = this.props.match.params.ndbno;
-    this.props.getReport(this.urlNDBNO);
-  }
+export const ReportPage = ({
+  match,
+  report,
+  getReport,
+  compareInsert,
+  setMass,
+  mass,
+  loaded,
+}) => {
+  const { name, ds, manu, ndbno } = get(report, "desc", {});
+  const urlNDBNO = match.params.ndbno;
 
-  sliderPrecision = (mass) => {
+  useEffect(() => {
+    getReport(urlNDBNO);
+  }, [urlNDBNO, getReport]);
+
+  const sliderPrecision = (() => {
     if (mass < 25) return 1;
     if (mass < 100) return 5;
     if (mass < 500) return 10;
     return 50;
-  };
+  })();
 
-  render() {
-    const { compareInsert, ndbno, mass } = this.props;
-    return (
-      <div className="ReportPage">
-        <div className="header">
-          <div>
-            <h1>
-              {this.props.loaded
-                ? `${this.props.name}`
-                : `Loading report for ndbno ${this.urlNDBNO}`}
-              <button onClick={() => compareInsert(ndbno, mass)}>
-                Compare
-              </button>
-            </h1>
-            <div className="descriptor">
-              {this.props.loaded && (
-                <small>
-                  {[this.props.ds, this.props.manu, this.props.ndbno]
-                    .filter((e) => e)
-                    .join(" · ")}
-                </small>
-              )}
-            </div>
-            {this.props.loaded && <Breadcrumbs name={this.props.name} />}
-            <Link to="/search/" className="newSearchLink">
-              &#8592; New Search
-            </Link>
-            <InputRange
-              className="massInput"
-              minValue={0}
-              maxValue={1000}
-              step={this.sliderPrecision(this.props.mass)}
-              value={this.props.mass}
-              onChange={this.props.setMass}
-              formatLabel={(value) => `Mass ${value} g`}
-            />
+  return (
+    <div className="ReportPage">
+      <div className="header">
+        <div>
+          <h1>
+            {loaded ? `${name}` : `Loading report for ndbno ${urlNDBNO}`}
+            <button onClick={() => compareInsert(ndbno, mass)}>Compare</button>
+          </h1>
+          <div className="descriptor">
+            {loaded && (
+              <small>{[ds, manu, ndbno].filter((e) => e).join(" · ")}</small>
+            )}
+          </div>
+          {loaded && <Breadcrumbs name={name} />}
+          <Link to="/search/" className="newSearchLink">
+            &#8592; New Search
+          </Link>
+          <InputRange
+            min={0}
+            max={1000}
+            step={sliderPrecision}
+            value={mass}
+            onChange={(_event, value) => setMass(value)}
+            valueLabelFormat={(value) => `Mass ${value} g`}
+          />
+        </div>
+      </div>
+      {loaded ? (
+        <div className="ReportPage-container">
+          <div className="ReportPage-report">
+            <Report report={report} />
+          </div>
+          <div className="ReportPage-analysis">
+            <Analysis report={report} />
           </div>
         </div>
-        {this.props.loaded ? (
-          <div className="ReportPage-container">
-            <div className="ReportPage-report">
-              <Report report={this.props.report} />
-            </div>
-            <div className="ReportPage-analysis">
-              <Analysis report={this.props.report} />
-            </div>
-          </div>
-        ) : (
-          <Loading />
-        )}
-      </div>
-    );
-  }
-}
+      ) : (
+        <Loading />
+      )}
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
   loaded: !isUndefined(state.app.getReportResult),
   report: state.app.getReportResult,
-  name: get(state.app.getReportResult, "desc.name"),
-  ds: get(state.app.getReportResult, "desc.ds"),
-  manu: get(state.app.getReportResult, "desc.manu"),
-  ndbno: get(state.app.getReportResult, "desc.ndbno"),
   mass: state.app.mass,
 });
 
