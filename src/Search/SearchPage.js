cfbddf02e2bd94a914fad583220f1eb990eceb02
westwood846 <PapstDonB@Googlemail.com>
2020-05-30 12:32:13 +0800

    Lazy-load results component

--- src/Search/SearchPage.js
+++ src/Search/SearchPage.js
@@ -1,59 +1,70 @@
-import React, { Component } from "react";
+import React, { Suspense, useEffect } from "react";
 import { connect } from "react-redux";
-import SearchResult from "./SearchResult";
 import SearchBar from "./SearchBar";
 import { setQuery, setDataSource, search } from "../actions";
 import Logo from "../Logo";
 import { decodeDataSourceIdentifier } from "../usda";
+import LazyLoadingFallback from "../common/LazyLoadingFallback";
 
-class SearchPage extends Component {
-  constructor(props) {
-    super(props);
+const SearchResult = React.lazy(() => import("./SearchResult"));
+
+const SearchPage = ({
+  location,
+  dataSource,
+  setDataSource,
+  query,
+  setQuery,
+  search,
+  searching,
+  error,
+  result,
+}) => {
+  useEffect(() => {
     let urlParams = new URLSearchParams(window.location.search);
+
     if (
       urlParams.has("dataSource") &&
-      urlParams.get("dataSource") !== this.props.dataSource
+      urlParams.get("dataSource") !== dataSource
     ) {
-      this.props.setDataSource(urlParams.get("dataSource"));
+      setDataSource(urlParams.get("dataSource"));
     }
     if (urlParams.has("query")) {
-      if (urlParams.get("query") !== this.props.query) {
-        this.props.setQuery(urlParams.get("query"));
-        this.props.search();
+      if (urlParams.get("query") !== query) {
+        setQuery(urlParams.get("query"));
+        search();
       }
     } else {
-      this.props.setQuery("");
+      setQuery("");
     }
-  }
+  }, [location, dataSource, setDataSource, query, setQuery, search]);
 
-  render() {
-    let result = this.props.searching ? (
-      <span>
-        Searching for "{this.props.query}" in{" "}
-        {decodeDataSourceIdentifier(this.props.dataSource)}...
-      </span>
-    ) : this.props.error ? (
-      this.props.error.errors.error.map((error, index) => (
-        <div key={index} style={{ color: "red" }}>
-          {error.message}
-        </div>
-      ))
-    ) : this.props.result ? (
-      <SearchResult result={this.props.result} />
-    ) : null;
-
-    return (
-      <div className="SearchPage">
-        <div className="header">
-          <Logo />
-          <SearchBar />
-        </div>
-        <hr />
-        <div className="ResultContainer">{result}</div>
+  let renderResult = searching ? (
+    <span>
+      Searching for "{query}" in {decodeDataSourceIdentifier(dataSource)}...
+    </span>
+  ) : error ? (
+    error.errors.error.map((error, index) => (
+      <div key={index} style={{ color: "red" }}>
+        {error.message}
       </div>
-    );
-  }
-}
+    ))
+  ) : result ? (
+    <Suspense fallback={<LazyLoadingFallback />}>
+      <SearchResult result={result} />
+    </Suspense>
+  ) : null;
+
+  return (
+    <div className="SearchPage">
+      <div className="header">
+        <Logo />
+        <SearchBar />
+      </div>
+      <hr />
+      <div className="ResultContainer">{renderResult}</div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
   searching: state.app.searching,
---
+++ src/common/LazyLoadingFallback.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const LazyLoadingFallback = () => {
+  return <div>Loading...</div>;
+};
+
+export default LazyLoadingFallback;
